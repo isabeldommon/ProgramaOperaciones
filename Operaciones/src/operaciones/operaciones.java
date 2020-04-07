667527e8564a74929380f06b19af64bae815ae6b
Isabel <isabeldommon@gmail.com>
2020-04-07 13:04:18 +0200

    Estado inicial

---
+++ Operaciones/src/operaciones/operaciones.java
@@ -0,0 +1,30 @@
+package operaciones;
+
+import java.util.*;
+
+
+public class operaciones {
+	public static void main(String[] args){
+		Scanner input= new Scanner (System.in);
+		
+		dividir(input);
+		
+	}
+
+	public static void dividir(Scanner input) {
+		try{
+			System.out.println("Introduzca el dividendo: ");
+			int n1 = input.nextInt();
+			System.out.println("Introduzca el divisor: ");
+			int n2 = input.nextInt();
+			int division = n1/n2;
+			System.out.println(division);
+
+		}
+		
+		catch(Exception e){
+			System.out.println("Pruebe de nuevo, por favor");
+		}
+	}
+
+}
